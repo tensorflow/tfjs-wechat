ef870f1614c8faabf3de933da26eba228b25647e
Ping Yu <4018+pyu10055@users.noreply.github.com>
2019-07-13 10:38:11 -0700

    updated demo urls

--- demo/posenet/pages/index/index.ts
+++ demo/posenet/pages/index/index.ts
@@ -19,7 +19,7 @@ import * as posenet from '@tensorflow-models/posenet';
 import {detectPoseInRealTime, drawPoses} from '../../posenet/posenet';
 const CANVAS_ID = 'image';
 const POSENET_URL =
-    'https://7465-tensorflowjs-e2061d-1259050850.tcb.qcloud.la/posenet/mobilenet/model.json';
+    'https://www.gstaticcnapps.cn/tfjs-models/savedmodel/posenet/mobilenet/float/050/model-stride16.json';
 Page({
   data: {result: ''},
   posenetModel: undefined,
